bd839e0db61f9f6f54d76c6c0cb2bc9174bfbbc5
Baik <bcy0729@naver.com>
2018-07-24 16:30:15 +0900

    header / about 레이아웃 수정

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
      *  UI 스크립트
      */
 
-    $('.main-header.btn').click(function (){
+    $('.main-header .btn').click(function (){
         $('.side-menu').addClass("is-active");
     })
 
@@ -13,7 +13,13 @@ $(document).ready(function() {
         $(".side-menu").removeClass("is-active");
     })
 
+    $('.scroll-down .scroll-toggle').click(function () {
+        $('.scroll-down .scroll-toggle').addClass('is`-active');
+        $('html, body').animate({scrollTop:850},'50');
 
+    })
 
 });
+
+
 
